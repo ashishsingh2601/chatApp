642bf91c920fe44b1ba2d945ed7f8bf69a741bb9
Ashish <as7220004@gmail.com>
2020-06-20 19:25:35 +0530

    Socket.io events added for messaging.

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,13 @@
 //jshint esversion:8
 
-io();
+const socket = io();
+    socket.on('message', (message)=>{
+        console.log(message);
+    });
+    document.querySelector('#message-form').addEventListener('submit', (e)=>{
+        e.preventDefault();                                                         //To avoid full page reload on submission
+        const message = e.target.elements.message.value;
+        socket.emit('sendMessage', message);
+    });
+
+
--- src/index.js
+++ src/index.js
@@ -14,8 +14,13 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', ()=>{
+io.on('connection', (socket)=>{
     console.log('New web socket connection!');
+
+    socket.emit('message', 'Welcome!');
+    socket.on('sendMessage', (message)=>{
+        io.emit('message', message);
+    });
 });
 
 server.listen(port, ()=>{
