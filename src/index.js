085e24393c4a7d9eed9ccbdd43e3862ec0748194
Ashish <as7220004@gmail.com>
2020-06-20 14:19:55 +0530

    Express server setup.

---
+++ src/index.js
@@ -0,0 +1,15 @@
+//jshint esversion:8
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectoryPath));
+
+app.listen(port, ()=>{
+    console.log(`Server is up on port ${port}!`);
+});
