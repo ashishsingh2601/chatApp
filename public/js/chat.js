3093fc530c7ab6e4456cce84ca5465fc4fbf9c49
Ashish <as7220004@gmail.com>
2020-06-21 19:11:37 +0530

    Form and button states improved.

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,13 +1,25 @@
 //jshint esversion:8
 
 const socket = io();
+
+const messageForm = document.querySelector('#message-form');
+const messageFormInput = messageForm.querySelector('input');
+const messageFormButton = messageForm.querySelector('button');
+const sendLocationButton = document.querySelector('#location');
+
     socket.on('message', (message)=>{
         console.log(message);
     });
-    document.querySelector('#message-form').addEventListener('submit', (e)=>{
-        e.preventDefault();                                                         //To avoid full page reload on submission
+    messageForm.addEventListener('submit', (e)=>{
+        e.preventDefault();                             //To avoid full page reload on submission
+        
+        messageFormButton.setAttribute('disabled', 'disabled');
+        
         const message = e.target.elements.message.value;
         socket.emit('sendMessage', message, (msg)=>{
+        messageFormButton.removeAttribute('disabled');
+        messageFormInput.value = '';
+        messageFormInput.focus();
         //    if(error){
         //        return console.log(error);
         //    }
@@ -15,13 +27,14 @@ const socket = io();
         });
     });
 
-    document.querySelector('#location').addEventListener('click', ()=>{
+    sendLocationButton.addEventListener('click', ()=>{
        if(!navigator.geolocation){
            return alert('Service is not supported by your browser!');
        }
-
+       sendLocationButton.setAttribute('disabled', 'disabled');
        navigator.geolocation.getCurrentPosition((position)=>{
         socket.emit('sendLocation', {latitude: position.coords.latitude , longitude: position.coords.longitude}, ()=>{
+            sendLocationButton.removeAttribute('disabled');
             console.log('Location Shared!');
         });
     });
