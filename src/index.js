fd9bf96fe2df09bc62205fd7cf28c3283d589730
Ashish <as7220004@gmail.com>
2020-06-22 19:20:29 +0530

    Time stamps added.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const express = require('express');
 const path = require('path');
 const socketio = require('socket.io');
 const filter = require('bad-words');
+const { generateMessage, generateLocationMessage } = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);                      //Creating server outside express library 
@@ -18,8 +19,8 @@ app.use(express.static(publicDirectoryPath));
 io.on('connection', (socket)=>{
     console.log('New web socket connection!');
 
-    socket.emit('message', 'Welcome!');
-    socket.broadcast.emit('message', 'A new user has joined!');
+    socket.emit('message', generateMessage('Welcome!'));
+    socket.broadcast.emit('message', generateMessage('A new user has joined!'));
 
     socket.on('sendMessage', (message, callback)=>{
         //const filter = new Filter()
@@ -27,16 +28,16 @@ io.on('connection', (socket)=>{
         //     return callback('Bad words not allowed!');
         // }
 
-        io.emit('message', message);
+        io.emit('message', generateMessage(message));
         callback('Message Delivered!');
     });
 
     socket.on('disconnect', ()=>{
-        io.emit('message', 'A user has left!');
+        io.emit('message', generateMessage('A user has left!'));
     });
     
     socket.on('sendLocation', (coords, callback)=>{
-        io.emit('locationMessage', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`);
+        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`));
         callback();
     });
 });
---
+++ src/utils/messages.js
@@ -0,0 +1,21 @@
+//jshint esversion: 9
+
+const generateMessage = (text)=>{
+    return{
+        text: text,
+        createdAt: new Date().getTime()
+    };
+};
+
+const generateLocationMessage = (url)=>{
+    return{
+        url: url,
+        createdAt: new Date().getTime()
+    };
+};
+
+
+module.exports = {
+    generateMessage,
+    generateLocationMessage
+};
